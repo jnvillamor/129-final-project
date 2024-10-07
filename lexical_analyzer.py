10ffdcad6c87e3a927e0d183c880f4acc8d06e03
-m <villamorjohnnoel@gmail.com>
2024-10-07 11:35:54 +0800

    initialize LexicalAnalyzer class

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,1 +1,10 @@
-# Lexical Analyzer 
+class LexicalAnalyzer: 
+  
+  def __init__(self):
+    # Set up the keywords and operators
+    keyword_file = open("keywords.txt", "r")
+    self.keywords = keyword_file.read().splitlines()
+    print(self.keywords)
+
+if __name__ == "__main__":
+  lexical_analyzer = LexicalAnalyzer()
