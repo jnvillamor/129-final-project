4ec4f1fe7f5aa2c4e640505d6baf0ef14ac86067
10lite <rcpaderna@up.edu.ph>
2024-11-07 13:43:33 +0800

    init: PE03 parser

--- PE03/parser.py
+++ PE03/parser.py
@@ -8,3 +8,44 @@
 # 3. Action: The action taken by the parser
 
 # Parser starts here
+
+class Parser:
+  def __init__(self):
+    self.prod_table = None
+    self.parse_table = None 
+   
+    self.stack = []
+    self.input_buffer = []
+    self.action = []
+    self.total_output = []
+    
+    self.is_valid = bool
+
+  def getInput(self, input_file):
+    if input_file.endswith('.prod'):
+      # File Validation
+      self.prod_table = input_file
+    
+    if input_file.endswith('.ptbl'):
+      # File validation
+      self.parse_table = input_file
+      
+    # If invalid input file, return error message
+    if not (input_file.endswith('.prod') or input_file.endswith('.ptbl')):
+      print('Error: Input file is not a .prod or .ptbl file')
+      return("Error - invalid input file")
+  
+    # Return input status message
+    print("LOADED: " + input_file)
+    return("Loaded: " + input_file)    
+   
+  def parse(self, input_string):
+    
+    pass
+  
+  def exportOutput(self):
+    # Append to total_output 
+    
+    # Export to .prsd file
+    
+    pass
