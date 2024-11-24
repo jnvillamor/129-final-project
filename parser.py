37f7b9bf84eaebd627749019586f43cd289d001b
-m <villamorjohnnoel@gmail.com>
2024-11-24 21:51:12 +0800

    fix: fix the implementation of syntax analysis

--- parser.py
+++ parser.py
@@ -93,67 +93,65 @@ class Parser:
       print(f'Error: {input_string} not found')
       return("Error - file not found")
 
-    for line in input[1:-1]:
-      # Initialize the input buffer and stack
-      self.input_buffer = line.strip().split(' ')
-      self.input_buffer = ['IOL'] + self.input_buffer + ['LOI']
-      self.stack.append('$')
-      self.stack.append(self.prod_table[0][1])
-      
-      # Add to the total output the initial
-      self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', ''])
-      # Get the current symbol and input
-      current_symbol = self.stack.pop()
-      current_input = self.input_buffer[0]
+    input = ''.join(input)
+    # Initialize the input buffer and stack
+    self.input_buffer = input.strip().split(' ')
+    self.stack.append('$')
+    self.stack.append(self.prod_table[0][1])
     
-      # Parse the input string 
-      while (current_symbol != '$' and len(self.stack) != 0):
-        print(self.total_output[-1])
-        # If the symbol and input match, pop the stack and input buffer
-        if(current_symbol == current_input):
-          self.action.append(f'Match {current_symbol}')
-          self.input_buffer.pop(0)
-          self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
-          
-          current_symbol = self.stack.pop()
-          current_input = self.input_buffer[0] if len(self.input_buffer) != 0 else '$'
-          
-
-        # If the symbol is a terminal and does not match the input, add an error
-        elif(current_symbol.isupper() and current_symbol != current_input):
-          self.action.append('Error: Terminal mismatch')
-          self.error_message = 'Terminal mismatch'
-          self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
-          self.is_valid = False
-          break
-        
-        elif(not current_symbol.isupper()):
-          # Get the production from the parse table
-          parse_row = self.getParseRow(current_symbol)
-          parse_col = self.getParseCol(current_input)
-          parse_cell = self.parse_table[parse_row][parse_col]
-          if parse_cell == '':
-            self.is_valid = False
-            self.action.append('Error: No production found')
-            self.error_message = 'No production found'
-            self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
-            break
-
-          production = self.prod_table[int(parse_cell) - 1][2]
-
-          for symbol in production.strip().split(' ')[::-1]:
-            self.stack.append(symbol) if symbol != 'e' else None
-          self.action.append(f'Output {current_symbol} > {production}')
-          self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
-          current_symbol = self.stack.pop()
-
-      # If the stack is empty, the input is valid
-      if len(self.stack) == 0:
-        self.is_valid = True
-        self.action.append('Match $')
-        self.total_output.append(['', '', self.action[-1]])
-      
+    # Add to the total output the initial
+    self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', ''])
+    # Get the current symbol and input
+    current_symbol = self.stack.pop()
+    current_input = self.input_buffer[0]
+  
+    # Parse the input string 
+    while (current_symbol != '$' and len(self.stack) != 0):
       print(self.total_output[-1])
+      # If the symbol and input match, pop the stack and input buffer
+      if(current_symbol == current_input):
+        self.action.append(f'Match {current_symbol}')
+        self.input_buffer.pop(0)
+        self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
+        
+        current_symbol = self.stack.pop()
+        current_input = self.input_buffer[0] if len(self.input_buffer) != 0 else '$'
+
+      # If the symbol is a terminal and does not match the input, add an error
+      elif(current_symbol.isupper() and current_symbol != current_input):
+        self.action.append('Error: Terminal mismatch')
+        self.error_message = 'Terminal mismatch'
+        self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
+        self.is_valid = False
+        break
+      
+      elif(not current_symbol.isupper()):
+        # Get the production from the parse table
+        parse_row = self.getParseRow(current_symbol)
+        parse_col = self.getParseCol(current_input)
+        parse_cell = self.parse_table[parse_row][parse_col]
+        if parse_cell == '':
+          self.is_valid = False
+          self.action.append('Error: No production found')
+          self.error_message = 'No production found'
+          self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
+          break
+
+        production = self.prod_table[int(parse_cell) - 1][2]
+
+        for symbol in production.strip().split(' ')[::-1]:
+          self.stack.append(symbol) if symbol != 'e' else None
+        self.action.append(f'Output {current_symbol} > {production}')
+        self.total_output.append([' '.join(self.stack[::-1]), ' '.join(self.input_buffer) + '$', self.action[-1]])
+        current_symbol = self.stack.pop()
+
+    # If the stack is empty, the input is valid
+    if len(self.stack) == 0:
+      self.is_valid = True
+      self.action.append('Match $')
+      self.total_output.append(['', '', self.action[-1]])
+    
+    print(self.total_output[-1])
 
     return (self.is_valid, self.error_message, self.total_output)
 
