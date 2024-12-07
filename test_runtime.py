f04dd7ee6d655aca1e8b178d75a2f271f73ea506
-m <villamorjohnnoel@gmail.com>
2024-12-08 04:21:29 +0800

    feat: operation in run time is running smoothly

--- test_runtime.py
+++ test_runtime.py
@@ -12,47 +12,72 @@ class Runtime:
 
     self.arithmetic_operators = ["ADD", "SUB", "MULT", "DIV"]
 
-  def get_next_token(self):
+  def _get_next_token(self):
     commands = self.tokens[self.line_number]
 
     if self.token_index >= len(commands):
       self.line_number += 1
       self.token_index = 0
-      return self.get_next_token()
+      return self._get_next_token()
 
     # Update the token index
     self.token_index += 1
     return commands[self.token_index - 1]
     
-  def process_arithmetic_operator(self, operator: str, op1: dict, op2: dict):
+  def _process_arithmetic_operator(self, current_operator: str):
+    # Get the first operand
+    op1 = self._get_next_token()
+
+    # If the operand is a variable, get its value from the symbol table
     if op1["name"] == "IDENT":
-      op1_value = self.symbol_table.get_symbol(op1["value"])["value"]
       op1_type = self.symbol_table.get_symbol(op1["value"])["type"]
-    else:
-      op1_value = op1["value"]
-      op1_type = "INT" if op1["name"] == "INTLIT" else "STR"
+      # if type is not int, raise an error
+      if op1_type != "INT":
+        raise Exception(f"Error: Operand {op1['value']} is not an integer")
+      op1_value = int(self.symbol_table.get_symbol(op1["value"])["value"])
 
+    # If the operand is an integer literal, get its value
+    elif op1["name"] == "INT_LIT":
+      op1_value = int(op1["value"])
+
+    # if the operand is an arithmetic operator, get the result of the operation
+    elif op1["name"] in self.arithmetic_operators:
+      result = self._process_arithmetic_operator(op1["name"])
+      op1_value = result
+    
+    # Get the second operand
+    op2 = self._get_next_token()
+
+    # If the operand is a variable, get its value from the symbol table
     if op2["name"] == "IDENT":
-      op2_value = self.symbol_table.get_symbol(op2["value"])["value"]
       op2_type = self.symbol_table.get_symbol(op2["value"])["type"]
-    else:
-      op2_value = op2["value"]
-      op2_type = "INT" if op2["name"] == "INTLIT" else "STR"
+      # if type is not int, raise an error
+      if op2_type != "INT":
+        raise Exception(f"Error: Operand {op2['value']} is not an integer")
+      op2_value = int(self.symbol_table.get_symbol(op2["value"])["value"])
 
-    # Semantic analysis for the arithmetic operator
-    if op1_type == "STR" or op2_type == "STR":
-      raise Exception("Error: Arithmetic operation between string and integer")
+    # If the operand is an integer literal, get its value
+    elif op2["name"] == "INT_LIT":
+      op2_value = int(op2["value"])
 
-    # Perform the arithmetic operation
-    if operator == "ADD":
+    # if the operand is an arithmetic operator, get the result of the operation
+    elif op2["name"] in self.arithmetic_operators:
+      result = self._process_arithmetic_operator(op2["name"])
+      op2_value = result
+
+    # Perform the operation
+    if current_operator == "ADD":
       return op1_value + op2_value
-    elif operator == "SUB":
+    elif current_operator == "SUB":
       return op1_value - op2_value
-    elif operator == "MULT":
+    elif current_operator == "MULT":
       return op1_value * op2_value
-    elif operator == "DIV":
+    elif current_operator == "DIV":
       if op2_value == 0:
-        raise Exception("Error: Division by zero")
+        if op2["name"] == "IDENT":
+          raise Exception(f"Error at line {self.line_number}: Division by zero in variable {op2['value']}")
+        else:
+          raise Exception(f"Error at line {self.line_number}: Division by zero in integer literal {op2['value']}")
       return op1_value / op2_value
 
   def process_input_code(self, tokens: dict, symbol_table: SymbolTable):
@@ -62,16 +87,12 @@ class Runtime:
     self.line_number = 1
     self.token_index = 0
 
-    current_token = self.get_next_token()
+    current_token = self._get_next_token()
 
     while current_token["name"] != "LOI":
       if current_token["name"] in self.arithmetic_operators:
-        current_operator = current_token["name"]
-        op1 = self.get_next_token()
-        op2 = self.get_next_token()
-
-        result = self.process_arithmetic_operator(current_operator, op1, op2)
+        result = self._process_arithmetic_operator(current_token["name"])
         print(result)
-      
-      current_token = self.get_next_token()
+
+      current_token = self._get_next_token()
 
